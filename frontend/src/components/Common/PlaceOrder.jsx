689c8f5c0c429e6c232713faeac9ad6314af887a
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2025-09-14 16:21:58 +0530

    placeOrder popUp complete

--- frontend/src/components/Common/PlaceOrder.jsx
+++ frontend/src/components/Common/PlaceOrder.jsx
@@ -1,18 +1,47 @@
 import { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
+import { toast } from 'sonner';
 
 const PlaceOrder = ({handlePlaceOrderOpen, offer}) => {
   const [selectedPotionImage, setSelectedPotionImage] = useState("");
   const [quantity, setQuantity] = useState(1);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
-  useEffect(() => {
-    if(offer.potionImages && offer.potionImages.length > 0) {
-      setSelectedPotionImage(offer.potionImages[0].url);
-    }
-  }, [offer]);
+  // useEffect(() => {
+  //   if(offer.potionImages && offer.potionImages.length > 0) {
+  //     setSelectedPotionImage(offer.potionImages[0].altText);
+  //   }
+  // }, [offer]);
 
   const handleQuantityChange = (amount) => {
     setQuantity((prev) => Math.max(prev + amount, 1));
+  };
+
+  const calculateTotalPrice = () => {
+    let basePrice = offer.price;
+    if (selectedPotionImage === "Large") {
+      basePrice += offer.increment;
+    }
+    return basePrice * quantity;
+  };
+
+  const handleAddToCart = () => {
+    if(!selectedPotionImage){
+      toast.error("Please select the potion size.", {
+        duration: 1000,
+      });
+      return;
+    }
+    // setIsButtonDisabled(true);
+
+    setTimeout(() => {
+      toast.success("Added to the cart", {
+        duration: 2000,
+      });
+      setIsButtonDisabled(false);
+      setSelectedPotionImage("");
+      setQuantity(1);
+    });
   };
 
   return (
@@ -25,10 +54,11 @@ const PlaceOrder = ({handlePlaceOrderOpen, offer}) => {
                 <img src={offer.image} alt={offer.title} className="w-full h-[90px] md:h-[200px] object-cover object-center rounded-t-lg"/>
                 <h3 className="text:md md:text-xl font-semibold mb-2 mt-3">{offer.item}</h3>
                 <p className="text-gray-600 mb-1 text-sm md:text-md">{offer.description}</p>
-                <p className="text-lg font-bold text-orange-500">LKR {offer.price}</p>
-                <div className="grid grid-cols-2 gap-6 mb-1 mt-3 ml-2">
+                <p className="text-md font-semibold text-orange-400">LKR {offer.price.toFixed(2)}</p>
+
+                <div className="grid grid-cols-2 gap-0 mb-1 mt-3 ml-2">
                   <div>
-                    <p className="italic">Potion</p>
+                    <p className="tracking-tighter">Potion :</p>
                     <div className="flex gap-4 mt-2">
                       {offer.potionImages.map((img, index) => (
                         <div key={index}>
@@ -36,24 +66,30 @@ const PlaceOrder = ({handlePlaceOrderOpen, offer}) => {
                             src={img.url} 
                             alt={img.altText} 
                             className={`w-[80px] h-[50px] md:h-[80px] object-cover border-2 object-center rounded-md cursor-pointer transition
-                              ${selectedPotionImage === img.url ? "border-blue-950" : "border-gray-400"}`}
-                            onClick={() => setSelectedPotionImage(img.url)}
+                              ${selectedPotionImage === img.altText ? "border-blue-950" : "border-gray-400"}`}
+                            onClick={() => {setSelectedPotionImage(img.altText); setIsButtonDisabled(false);}}
                           />
                           <p className="text-sm">{img.altText}</p>
+                          {img.altText === "Large" && (
+                            <p className="text-xs tracking-tighter">+ LKR {offer.increment.toFixed(2)}</p>
+                          )}
                         </div>
                       ))}
                     </div>
                   </div>
 
                   <div>
-                    <p className="italic">Amount</p>
-                    <div className="flex items-center mt-2">
+                    <div className="flex justify-start mt-2">
+                      <p className="tracking-tighter mr-4 mt-1">Quantity :</p>
                       <button onClick={() => handleQuantityChange(-1)} className="border rounded-lg px-2 text-xl font-medium">-</button>
                       <span className="font-semibold m-1">{quantity}</span>
                       <button onClick={() => handleQuantityChange(1)} className="border rounded-lg px-2 text-xl font-medium">+</button>
                     </div>
-                    <div className="flex flex-col mb-3 mt-3">
-                      <button className="bg-orange-500 text-white py-2 px-4 rounded mr-4">Add to Cart</button>
+                    <div className="flex justify-start mt-4">
+                      <p className="text-lg font-bold text-orange-500">LKR {calculateTotalPrice().toFixed(2)}</p>
+                    </div>
+                    <div onClick={handleAddToCart} className="flex flex-col mt-4">
+                      <button className={`text-white py-2 px-4 rounded mr-4 ${isButtonDisabled ? "bg-orange-400 cursor-not-allowed" : "bg-orange-500 hover:bg-orange-600"}`}>Add to Cart</button>
                     </div>
                   </div>
 
