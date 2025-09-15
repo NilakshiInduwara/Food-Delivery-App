572ef7858815a6b07c1fea7a01379013fdb4969a
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2025-09-16 01:36:36 +0530

    filter sidebar

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../Common/Navbar'
 import SideBar from '../Common/SideBar'
+import FilterSideBar from '../Common/FilterSideBar'
 
 const UserLayout = () => {
   return (
     <>
       <Navbar />
-      <SideBar />
+      {/* <SideBar /> */}
+      <FilterSideBar/>
       <main>
         <Outlet />      {/* Render the matched child route component in APP.jsx */}
       </main>
