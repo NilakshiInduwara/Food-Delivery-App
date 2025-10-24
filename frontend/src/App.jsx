feb7f771916d258c669e5a1ea323e939e28c35cf
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2025-10-25 02:42:06 +0530

    images upload - FE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import UserLayout from './components/Layout/UserLayout'
+import RestaurantLayout from './components/Layout/RestaurantLayout'
 import Home from './pages/Home'
 import TodaysOffers from './pages/TodaysOffers'
 import { Toaster } from "sonner";
+import OfferForm from './pages/OfferForm';
 
 const App = () => {
   return (
@@ -12,7 +14,10 @@ const App = () => {
         <Route path="/" element={<UserLayout />} >
           <Route index element={<Home />} />
           <Route path="/offers" element={< TodaysOffers/>} />
-        </Route>        
+        </Route>       
+        <Route path="/restaurant" element={<RestaurantLayout />} >
+          <Route path="/restaurant/createOffer" element={< OfferForm/>} />
+        </Route> 
       </Routes>
     </BrowserRouter>
   )
