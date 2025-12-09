3904c0583d18919b5a56fa74b2c0eeb598bf6e6d
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2025-12-09 23:50:03 +0530

    Food items related functions - BE

--- backend/controllers/restaurantController.js
+++ backend/controllers/restaurantController.js
@@ -1,3 +1,4 @@
+import FoodItem from "../models/FoodItem.js";
 import Restaurant from "../models/Restaurant.js";
 
 export const getRestaurants = async (req, res) => {
@@ -6,6 +7,16 @@ export const getRestaurants = async (req, res) => {
     res.json(restaurants);
   } catch (error) {
     console.error("Error fetching restaurants:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getfoodItems = async (req, res) => {
+  try {
+    const foodItems = await FoodItem.find();
+    res.json(foodItems);
+  } catch (err) {
+    console.error("Error fetching food items: ", err);
     res.status(500).json({ message: "Internal server error" });
   }
 };
@@ -24,4 +35,41 @@ export const createRestaurant = async (req, res) => {
     console.error("Error creating restaurant:", error);
     res.status(500).json({ message: "Internal server error" });
   }
+};
+
+export const createFoodItem = async (req, res) => {
+    try {
+        const { name, item, price, title, description 
+          // ,validTill, increment 
+        } = req.body;
+
+        if (!name || !item || !price || !title || !description) {
+          return res.status(400).json({ message: "All fields are required" });
+        }
+
+        // Handle main image
+        const image = req.files.image ? `/uploads/${req.files.image[0].filename}` : null;
+
+        // Handle potion images
+        const potionImages = req.files.potionImages ? req.files.potionImages.map(file => ({
+            url: `/uploads/${file.filename}`,
+            altText: file.originalname
+        })) : [];
+
+        const foodItem = await FoodItem.create({
+            name,
+            item,
+            price,
+            title,
+            description,
+            // validTill,
+            // increment,
+            image,
+            potionImages,
+        });
+
+        res.status(201).json(foodItem);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 };
